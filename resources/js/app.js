21c90d89dbff1b6365c6094785eb5e8d5230cfea
Carlos Vazquez <juancarlosfrausto52@outlook.com>
2023-05-04 20:56:14 -0500

    Post factory

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import Dropzone from "dropzone";
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone',{
+    dictDefaultMessage:'Da clic para subir fotografía',
+    acceptedFiles:'.png,.jpg,.jpeg,.gif',
+    addRemoveLinks:true,
+    dictRemoveFile:'Borrar fotografía',
+    maxFiles:1,
+    uploadMultiple:false,
+});
+
+dropzone.on('success',function(file,response){
+console.log(response);
+});
---
+++ resources/views/publicacion/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('titulo')
+    Crear nueva publicación
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
+@section('contenido')
+    <div class="md:flex md:items-center mx-5">
+        <div class="md:w-1/2 px-10">
+            <form action="{{ route('imagenes.store') }}" id="dropzone"
+                class="dropzone border-dashed border-2 w-full h-72 rounded flex flex-col justify-center items-center bg-transparent"
+                enctype="multipart/form-data">
+                @csrf
+
+            </form>
+        </div>
+        <div class="md:w-1/2 px-10 w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label class="mb-2 block text-gray-700 font-bold uppercase" for="titulo">Título</label>
+                    <input class="border p-1 w-full rounded-lg @error('titulo') border-red-500 @enderror" type="text"
+                        id="titulo" name="titulo" value="{{ old('titulo') }}"
+                        placeholder="@error('titulo') {{ $message }} @enderror">
+                </div>
+
+                <div class="mb-5">
+                    <label class="mb-2 block text-gray-700 font-bold uppercase" for="titulo">Descripción</label>
+                    <textarea class="border p-1 w-full rounded-lg @error('descripcion') border-red-500 @enderror" type="text"
+                        id="descripcion" name="descripcion" placeholder="@error('descripcion') {{ $message }} @enderror">
+                        {{ old('descripcion') }}
+                    </textarea>
+                </div>
+                <input
+                    class="cursor-pointer hover:bg-sky-700 uppercase w-full rounded-lg font-bold bg-sky-600 text-white p-2"
+                    type="submit" value="Publicar">
+            </form>
+        </div>
+    </div>
+    </div>
+@endsection
